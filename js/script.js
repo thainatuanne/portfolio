b8a697c3e654eda7c3a7f9a321c026bac09c6083
thainatuanne <thaina-tuanne@hotmail.com>
2024-12-14 12:13:20 -0300

    alteraçoes e adições + sobre

--- js/script.js
+++ js/script.js
@@ -5,3 +5,21 @@ menuIcon.onclick = () => {
     navLinks.classList.toggle('active');
 
 }
+
+// texto animado
+
+document.addEventListener("DOMContentLoaded", () => {
+    const textos = document.querySelectorAll(".texto-animado");
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("ativo");
+            }
+        });
+    });
+
+    textos.forEach((texto) => {
+        observer.observe(texto);
+    });
+});
